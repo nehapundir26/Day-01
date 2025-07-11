ffa0b9c6106a9548b88c0702f467482f3fed09d9
Neha Pundir <nehapundir515@gmail.com>
2025-07-11 15:28:52 +0530

    Second largest number

---
+++ c++ 1st/secondlarge.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <climits> // for INT_MIN
+using namespace std;
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    int first = INT_MIN;
+    int second = INT_MIN;
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > first) {
+            second = first;
+            first = arr[i];
+        } else if (arr[i] > second && arr[i] != first) {
+            second = arr[i];
+        }
+    }
+
+    if (second == INT_MIN)
+        cout << "No second largest element exists." << endl;
+    else
+        cout << "Second largest element is: " << second << endl;
+
+    return 0;
+}
