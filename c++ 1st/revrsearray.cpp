0c9322fdfbf40ead794a5b62930d443b185798f2
Neha Pundir <nehapundir515@gmail.com>
2025-07-11 15:25:57 +0530

    Added the reverse of array

---
+++ c++ 1st/revrsearray.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    // Reversing the array manually
+    for (int i = 0; i < n / 2; i++) {
+        int temp = arr[i];
+        arr[i] = arr[n - i - 1];
+        arr[n - i - 1] = temp;
+    }
+
+    // Printing the reversed array
+    cout << "Reversed array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
